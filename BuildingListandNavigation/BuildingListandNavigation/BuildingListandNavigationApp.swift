4231fc64df6bc3eec6d5c4249e6685b30a74b293
jaeseb96@naver.com <jaeseb96@naver.com>
2021-01-12 15:19:21 +0900

    2021.01.12 V0.4

---
+++ BuildingListandNavigation/BuildingListandNavigation/BuildingListandNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildingListandNavigationApp.swift
+//  BuildingListandNavigation
+//
+//  Created by JaeseopKim on 2021/01/12.
+//
+
+import SwiftUI
+
+@main
+struct BuildingListandNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # study_swift #
 
 * apple developer SwiftUI Tutorials를 보고 따라하는 SwiftUI 공부
+* https://developer.apple.com/tutorials/swiftui
 
 
