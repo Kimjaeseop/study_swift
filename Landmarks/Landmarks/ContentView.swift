a96a02d98ceaac9da93ecafdafe0a8573a6c95d9
jaeseb96@naver.com <jaeseb96@naver.com>
2021-01-12 13:25:04 +0900

    2021.01.12 V0.1

--- Landmarks/Landmarks/ContentView.swift
+++ Landmarks/Landmarks/ContentView.swift
@@ -9,19 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack (alignment: .leading){
-            Text("Hello, world!")
-                .font(.title)
-                .foregroundColor(.blue)
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer() // to use the full width of the device
-                Text("California")
-                    .font(.subheadline)
+        VStack {
+            MapView()
+                .frame(height: 300)
+    
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack (alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                    .foregroundColor(.blue)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer() // to use the full width of the device
+                    Text("California")
+                        .font(.subheadline)
+                }
             }
+            .padding()
         }
-        .padding()
     }
 }
 
---
+++ Landmarks/Landmarks/MapView.swift
@@ -0,0 +1,25 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by JaeseopKim on 2021/01/11.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+        span: MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+    )
+    var body: some View {
+        Map(coordinateRegion: $region)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
